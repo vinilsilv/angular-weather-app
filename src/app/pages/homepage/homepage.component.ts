5ecdc6985209486919dbaf9540ffa8412ad2d8fc
Vincius Silva <vinilrsilv@gmail.com>
2022-07-28 17:27:41 -0300

    feat[Global]: General styles and max and min temperatures

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 const API_URL = environment.API_URL;
 const API_KEY = environment.API_KEY;
@@ -14,28 +14,42 @@ export class HomepageComponent implements OnInit {
   weatherCityName!: string;
   weatherCityCountry!: string;
   weatherDescription!: string;
-  citySearch = 'paraiba';
+  maxTemp!: number;
+  minTemp!: number;
+  citySearch = '';
+  defaultCity = 'brasilia'
 
   loading = true;
 
-  constructor(public httpClient: HttpClient) {}
+  constructor(public httpClient: HttpClient) {
+  }
 
   ngOnInit(): void {
-    this.loadData();
+    this.loadData()
   }
 
   loadData() {
+    if(this.citySearch != ''){
+
+      this.getData(this.citySearch);
+    } else {
+      this.getData(this.defaultCity)
+    }
+  }
+
+  getData(city: string) {
     this.httpClient
-      .get(`${API_URL}weather?q=${this.citySearch}&appid=${API_KEY}`)
+      .get(`${API_URL}weather?q=${city}&appid=${API_KEY}`)
       .subscribe({
         next: (res: any) => {
+          console.log(res)
           this.loading = true;
           this.weatherTemp = res['main'];
-          this.weatherCityName = res.name
-          this.weatherCityCountry = res.sys.country
-          this.weatherDescription = res.weather[0].main
-          console.log(res);
-          console.log(this.weatherDescription);
+          this.weatherCityName = res.name;
+          this.weatherCityCountry = res.sys.country;
+          this.weatherDescription = res.weather[0].main;
+          this.maxTemp = res.main.temp_max
+          this.minTemp = res.main.temp_min
           this.loading = false;
         },
       });
